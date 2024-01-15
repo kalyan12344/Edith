dbe8ef8c885e202b975a8a65ee0dcc0abd1a361b
kalyan12344 <kalyanraju90@gmail.com>
2024-01-15 16:38:36 -0600

    chhanged root path

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
     <>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Chat />} />
+          <Route exact path="/Edith" element={<Chat />} />
         </Routes>
       </BrowserRouter>
     </>
